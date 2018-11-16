1206caa1170f7cf54d3cc3355d170026acc5794e
unknown <houxuyong@hotmail.com>
2018-11-16 10:49:57 +0800

    Message console impled.

--- src/org/ngo/eide/host/NgoEndpoint.java
+++ src/org/ngo/eide/host/NgoEndpoint.java
@@ -1,16 +1,28 @@
 package org.ngo.eide.host;
 
+import java.awt.Color;
 import java.util.HashMap;
 
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 import org.ngo.eide.commands.AddProjCommand;
 import org.ngo.eide.commands.Command;
 import org.ngo.eide.commands.ExitCommand;
 import org.ngo.eide.commands.MarkCommand;
 import org.ngo.eide.commands.MileStoneCommand;
+import org.ngo.eide.commands.TestCommand;
 import org.ngo.ether.endpoint.EndpointCallback;
 import org.ngo.ether.endpoint.EndpointSupport;
 import org.slf4j.Logger;
@@ -46,6 +58,7 @@ public class NgoEndpoint implements EndpointCallback {
 		COMMAND_MAP.put(Command.ADDPROJ, new AddProjCommand());
 		COMMAND_MAP.put(Command.MILESTONE, new MileStoneCommand());
 		COMMAND_MAP.put(Command.MARK, new MarkCommand());
+		COMMAND_MAP.put(Command.TEST, new TestCommand());
 
 		// loop until the workbench is instantiated
 		while (true) {
@@ -94,6 +107,8 @@ public class NgoEndpoint implements EndpointCallback {
 						String info = message.substring(eideCmd.length()+1);
 						
 						try {
+							writeToConsole(eideCmd);
+							writeToConsole(info);
 							Command cmdObj = COMMAND_MAP.get(eideCmd);
 							cmdObj.execute(info);
 							client.sendMessage(new EideResponse(eideCmd, "OK", "Done").toString(), NgoEndpoint.NGO_ID , NgoEndpoint.NGO_REMOTE_PEER_ID);
@@ -107,7 +122,44 @@ public class NgoEndpoint implements EndpointCallback {
 			client.sendMessage(EideResponse.EIDE_NOT_READY.toString(), NgoEndpoint.NGO_ID , NgoEndpoint.NGO_REMOTE_PEER_ID);
 		}
 	}
+	
+	private MessageConsole findConsole(String name) {
+	      ConsolePlugin plugin = ConsolePlugin.getDefault();
+	      IConsoleManager conMan = plugin.getConsoleManager();
+	      IConsole[] existing = conMan.getConsoles();
+	      for (int i = 0; i < existing.length; i++)
+	         if (name.equals(existing[i].getName()))
+	            return (MessageConsole) existing[i];
+	      //no console found, so create a new one
+	      MessageConsole myConsole = new MessageConsole(name, null);
+	      conMan.addConsoles(new IConsole[]{myConsole});
+	      return myConsole;
+   }
 
+	private static final String NGO_CONSOLE_NAME = "NGO_WebConsole";
+	/**
+	 * https://wiki.eclipse.org/FAQ_How_do_I_find_the_active_workbench_page%3F
+	 * @param message
+	 * @throws Exception 
+	 */
+	public void writeToConsole(String message) throws Exception {
+		 MessageConsole myConsole = findConsole(NGO_CONSOLE_NAME);
+		 MessageConsoleStream out = myConsole.newMessageStream();
+		 if (message.startsWith("$"))
+			 out.setColor(Display.getCurrent().getSystemColor(SWT.COLOR_RED));
+		 else 
+			 out.setColor(Display.getCurrent().getSystemColor(SWT.COLOR_BLACK));
+		 out.println(message);
+		 /*
+		 IWorkbench wb = PlatformUI.getWorkbench();
+		 IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		  // on new versions it may need to be changed to:
+		 IWorkbenchPage page = win.getActivePage();// obtain the active page
+		 String id = NGO_CONSOLE_NAME;//IConsoleConstants.ID_CONSOLE_VIEW;
+		 IConsoleView view = (IConsoleView) page.showView(id);
+		 view.display(myConsole);
+		 */
+	}
 
 	@Override
 	public void error(String message) {
