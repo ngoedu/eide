2ddf590f4f859e436d473b06721361293793e7be
unknown <houxuyong@hotmail.com>
2018-11-17 19:29:51 +0800

    AddProj logic change - add when not exists in ws, open it when exists, all others will be closed.

--- src/org/ngo/eide/commands/AddProjCommand.java
+++ src/org/ngo/eide/commands/AddProjCommand.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
@@ -17,6 +19,37 @@ public class AddProjCommand implements Command {
 
 	@Override
 	public int execute(String message) throws Exception{
+		// workspace
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		// root workspace
+		IWorkspaceRoot wsroot = workspace.getRoot();
+		
+		String projName = message.substring(message.lastIndexOf("\\")+1);
+		IProject currProject = wsroot.getProject(projName);
+		
+		//add project into ws if not yet.
+		if (!currProject.exists()) {
+			addProjectToWS(message);
+		}
+		
+		//open the target project
+		if (!currProject.isOpen())
+			currProject.open(null);
+		
+		//unrelated projects closure
+		IProject[] allProjects = wsroot.getProjects();
+		for(IProject p : allProjects) {
+			if (p.getName() != currProject.getName() && p.isOpen()) {
+				p.close(null);
+			}
+		}
+	
+		return NgoEndpoint.NGONAT_EIDECLIENT_ID;
+	}
+	
+	
+	private void addProjectToWS(String message) throws Exception{
+	
 		String projPath = message;
 		IProjectDescription description = ResourcesPlugin.getWorkspace().loadProjectDescription(new Path(projPath+"\\.project"));
 
@@ -33,8 +66,6 @@ public class AddProjCommand implements Command {
         importOperation.setCreateContainerStructure(false);
         importOperation.setCreateLinks(false);
         importOperation.run(new NullProgressMonitor());
-        
-        return NgoEndpoint.NGONAT_EIDECLIENT_ID;
         
 	}
 
